ff5b7ad13c959fbe83b9c3bc5ea648a4ef5d27aa
rbulin <roman.bulinski@pentacomp.pl>
2020-02-02 15:07:39 +0100

    Implemented code

--- src/main/java/Itro.java
+++ src/main/java/Itro.java
@@ -1,44 +1,34 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+import java.util.Arrays;
+
 public class Itro {
 
     public static String[] getW(int height) {
         if (height < 2) {
             return new String[0];
         }
-
-        String[] result = new String[height];
-            for (int i = 0; i < height; i++) {
-                String temp = "";
-                for (int j = 1; j < (height * 4) - 2; j++) {
+        int start = 0;
+        int end = (height*2)-2 ;
+        String[] halfResult = new String[height];
+        for (int i = 0; i < height; i++) {
+            String temp = "";
+            for (int j = 0; j < (height * 2)-1; j++) {
+                if( j == start || j == end ){
+                    temp = temp + "*";
+                }else{
                     temp = temp + " ";
                 }
-                result[i] = temp;
             }
-
-        int start = 0;
-        int middle = (result[0].length()-1) /2 ;
-        int end = result[0].length() - 1 ;
-
-        char[] temparr = result[0].toCharArray();
-        temparr[start] = '*';
-        temparr[middle] = '*';
-        temparr[end] = '*';
-
-        StringBuilder sb = new StringBuilder();
-        for( char c : temparr ){
-            sb.append(c);
+            start++;
+            end--;
+            halfResult[i] = temp + temp.substring(1);
         }
-        result[0] = sb.toString();
-
-        return result;
+        return halfResult;
     }
 
     public static void main(String[] args){
-
-
-
-
+        Arrays.asList( getW(7) ).stream().forEach( n-> System.out.println(n));
     }
 
 }
